9fad8a248a6c2afab6188980cd1df3c30e121d0f
Terence Hughes <Terencehughes450@gmail.com>
2024-01-26 18:06:07 -0500

    added a targetInputContainer variable and and entryNumber variable

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ function cleanInputString(str) {
   };
 
   function addEntry(){
-
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
   };
